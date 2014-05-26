d32004d1976f05b7bfd646b64e7d508474ea160f
rabidpraxis <webster.kevin@gmail.com>
2014-05-26 05:41:40 +0200

    Added basic_wire example

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(declare create-build)
 (defproject controlroom/idiomatic "0.1.0"
-  :description "How to use ctrlrm projects as they were intended"
+  :description "How to use ctrlrm projects"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -8,14 +7,13 @@
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
-  :cljsbuild {:builds
-   (map create-build ["basic_wire"])})
+  :cljsbuild
+  {:builds
+   [{:id "basic_wire"
+     :source-paths ["examples/basic_wire/src"]
+     :compiler {:output-dir "examples/basic_wire/out"
+                :output-to  "examples/basic_wire/main.js"
+                :optimizations  :none
+                :output-wrapper false
+                :source-map     true }}]})
 
-(defn create-build [id]
-  {:id id
-   :source-paths [(str "examples/" id "/src")]
-   :compiler {:output-dir (str "examples/" id "/out")
-              :output-to  (str "examples/" id "/main.js")
-              :optimizations  :none
-              :output-wrapper false
-              :source-map     true }})
